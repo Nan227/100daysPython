6545413ed19cc55372e6b81945830c8dd994306c
Piyaporn Puangprasert (Nan) <dngooddeals@gmail.com>
2023-01-22 23:18:56 -0500

    Update day2TipCal.py

--- day2TipCal.py
+++ day2TipCal.py
@@ -1,11 +1,12 @@
 #Tip-calculator app
 
 # User inputs 3 value ( Cost of the bill, percent of the Taxes, number of people)
+print('Welcome to the Tip-Calculator app \n')
 bill =input('How much the bill cost? ')
 percent = input('How much percentage of Taxes? ')
 split = input('How many people split the bill? ')
 
-#convert srt => number
+#convert str => number
 billF = float(bill)
 percentF = float(percent)
 splitI= int(split)
