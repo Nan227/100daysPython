6752f2846035665ba87ba43ab3d613a9bfef4d73
Nan227 <you@example.com>
2023-01-22 23:12:06 -0500

    Tip-Calculator app added

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-# 100daysPython
-Date 1/17/2023-2/28/2023
-Udemy.com
-
-# day 2 creat tip-calculator app
-- User input 3 valuable ( cost of the bill, % tax, and how many friends split)
-- The app will display the final bill. 
-=======
 # 100daysPython Udemy.com
 #Date plan 1/17/2023-2/28/2023
 
 #Day1 : type, round(), input(), srt()
 #Day2: tip-calculator project
 #Day3: if else
->>>>>>> ffb644c9005fdc959e5fdc1e687fa04e54e4de95
---
+++ day2TipCal.py
@@ -0,0 +1,17 @@
+#Tip-calculator app
+
+# User inputs 3 value ( Cost of the bill, percent of the Taxes, number of people)
+bill =input('How much the bill cost? ')
+percent = input('How much percentage of Taxes? ')
+split = input('How many people split the bill? ')
+
+#convert srt => number
+billF = float(bill)
+percentF = float(percent)
+splitI= int(split)
+
+#calculate
+totalbill = round(billF + (billF*percentF/100),2)
+eachPay = round(totalbill/splitI,2)
+print('\nYour total bill is '+ str(totalbill))
+print('\nEach person will pay '+ str(eachPay))
