ac48b3b6eff62905cde494d259e917df55aaee50
Nan227 <you@example.com>
2023-01-24 11:09:30 -0500

    Add pow function to the calculator

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 # User input 2 number and 1 operator
 num1 =float(input("Enter the first number: "))
-op = input("Enter the opperator ' +, -, *, / ' : ")
+op = input("Enter the opperator ' +, -, *, / , x^y' : ")
 num2 =float(input("Enter the first number: "))
 
 # calculate
@@ -12,6 +12,8 @@ elif op =="*":
   result = num1 * num2
 elif op =="/":
     result = num1 / num2
+elif op =="^":
+    result = pow(num1,num2)
 else: 
     print("No answer is not correct")
 print(str(num1)+ " "+ op + " "+ str(num2) + " = "+ str(round(result,2)))
