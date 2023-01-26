e0ad664034d0d6f12742a1fda5ce6a020197a98b
Nan227 <you@example.com>
2023-01-25 23:53:26 -0500

    update code on day3EvernOddNumber.py

--- day3EvenOddNumber.py
+++ day3EvenOddNumber.py
@@ -4,8 +4,7 @@ number = int(input("Which number do you want to check? "))
 # check with %
 if(number%2)==0:
   print(" Your number is "+ str(number)+ ", it is even number")
-elif(number%2)!=0:
-  print("\nYour number is "+ str(number)+ ", it is odd number")
 else:
-  print("The system error ")
+  print("\nYour number is "+ str(number)+ ", it is odd number")
+
 
