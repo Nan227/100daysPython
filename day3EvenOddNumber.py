c401dd516aa3a2191afcca690948ab6fc02edab3
Nan227 <you@example.com>
2023-01-26 00:14:07 -0500

    Add the roller coaster check age and high

--- day3EvenOddNumber.py
+++ day3EvenOddNumber.py
@@ -7,4 +7,15 @@ if(number%2)==0:
 else:
   print("\nYour number is "+ str(number)+ ", it is odd number")
 
+print("\nWelcome to roller coaster ride")
+print("This machine is allowed people who is higher than 120cm to play")
+high = float(input("How tall are you? "))
+if high >=120:
+  age= int(input("How old are you? "))
+  if age >18:
+    print("Cost of a ride roller coaster is $12")
+  else:
+    print("Cost of a ride roller coaster is $7")
+else:
+  print("Sorry, you can not play the roller coaster at this time")
 
