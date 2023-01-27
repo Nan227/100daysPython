bddf6001d0b77adb5e0735c1e9d0dc0d7a644671
Piyaporn Puangprasert (Nan) <dngooddeals@gmail.com>
2023-01-26 23:59:08 -0500

    Update day3BMI.py

--- day3BMI.py
+++ day3BMI.py
@@ -1,5 +1,5 @@
-#Greeting 
-print("Welcome to the BMI app")
+#Greeting with user
+print("Welcome to the Body Mass Index app")
 #User input weight(pound) and height(feet)
 weight = float(input("Enter your weight(pound): "))
 height = float(input("Enter your height(feet): "))
@@ -7,3 +7,11 @@ height = float(input("Enter your height(feet): "))
 #convert weight(pounds) to kilograms by division with 2.2
 
 #Covert  height(feet) to meters (1 foot= 0.3048)
+
+#Calculate the BMI
+
+#If BMI is under 18.5, they are Underweight
+#If 18.5 < BMI < 25, they are normal weight
+#If 25 < BMI < 30, they are overweight
+#If 30 < BMI < 35, they are obese
+#If Above
