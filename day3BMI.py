ea40d5a12f94c9929ce2044bf6f184b462dc59c6
Nan227 <you@example.com>
2023-01-26 23:52:52 -0500

    Add day3BMI.py

---
+++ day3BMI.py
@@ -0,0 +1,9 @@
+#Greeting 
+print("Welcome to the BMI app")
+#User input weight(pound) and height(feet)
+weight = float(input("Enter your weight(pound): "))
+height = float(input("Enter your height(feet): "))
+
+#convert weight(pounds) to kilograms by division with 2.2
+
+#Covert  height(feet) to meters (1 foot= 0.3048)
