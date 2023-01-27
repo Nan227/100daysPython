1d5846b616ada15b3d0530fe0f549115c2167b71
Nan227 <you@example.com>
2023-01-27 00:16:26 -0500

    modified day3BMI.py

--- day3BMI.py
+++ day3BMI.py
@@ -5,13 +5,25 @@ weight = float(input("Enter your weight(pound): "))
 height = float(input("Enter your height(feet): "))
 
 #convert weight(pounds) to kilograms by division with 2.2
-
 #Covert  height(feet) to meters (1 foot= 0.3048)
+weightKM = weight/2.2
+heightM = height * 0.3048
 
 #Calculate the BMI
+bmi = weightKM/(heightM*heightM)
 
 #If BMI is under 18.5, they are Underweight
 #If 18.5 < BMI < 25, they are normal weight
 #If 25 < BMI < 30, they are overweight
 #If 30 < BMI < 35, they are obese
 #If Above 35, they are clinically obese
+if bmi < 18.5:
+  print(" Your BMI is "+ str(round(bmi,2))+ ", and you are underweight")
+elif (bmi >= 18.5) and (bmi < 25):
+  print(" Your BMI is "+ str(round(bmi,2))+ ", and you are normalweight")
+elif (bmi >= 25) and (bmi < 30):
+  print(" Your BMI is "+ str(round(bmi,2))+ ", and you are overweight")
+elif (bmi >= 30) and (bmi < 35):
+  print(" Your BMI is "+ str(round(bmi,2))+ ", and you are obese")
+elif bmi >= 35:
+  print(" Your BMI is "+ str(round(bmi,2))+ ", and you are clinically obese")
