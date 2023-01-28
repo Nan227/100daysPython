bd0e31fed230fd8993d028e667ec8f82d37b64ea
Nan227 <you@example.com>
2023-01-28 01:31:25 -0500

    Fixed the answer from float to int

--- day3Leapyear.py
+++ day3Leapyear.py
@@ -1,12 +1,13 @@
-year= float(input("Enter the year(Leap year checker): "  ))
+year= int(input("Enter the year(Leap year checker): "  ))
 #3 steps checking , first must %4 ==0. Second, must % 100 !=0, last must % 400==0
-if year %4 == 0:
-  if year % 100 ==0:
-    if year %400 == 0:
-       print(f" Year {round(year,0)} is the leap year")
+yearF=float(year)
+if yearF %4 == 0:
+  if yearF % 100 ==0:
+    if yearF %400 == 0:
+       print(f" Year {year} is the leap year")
     else:
-          print(f" Year {round(year,0)} is not the leap year")
+          print(f" Year {year} is not the leap year")
   else:
-    print(f" Year {round(year,0)} is the leap year")
+    print(f" Year {year} is the leap year")
 else:
-  print(f" Year {round(year,0)} is not the leap year")
+  print(f" Year {year} is not the leap year")
