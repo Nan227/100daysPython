f197af08b7553fd318fec73d4e2f514aa5bc537b
Piyaporn Puangprasert (Nan) <dngooddeals@gmail.com>
2023-01-27 23:58:05 -0500

    Update day3Leapyear.py

--- day3Leapyear.py
+++ day3Leapyear.py
@@ -1,5 +1,5 @@
 year= float(input("Enter the year(Leap year checker): "  ))
-#3 steps checking , first must %4 ==0. Second, must % 100 !=0, last must % 400==0
+#3 steps checking , first the year have to %4 ==0. Second, the year have to % 100 ==0, last the year have to % 400==0
 if year %4 == 0:
   if year % 100 ==0:
     if year %400 == 0:
