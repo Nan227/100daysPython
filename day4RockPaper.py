695c79dd1d0cba5368b9b6d8c8cd22eff4d1ff23
Piyaporn Puangprasert (Nan) <dngooddeals@gmail.com>
2023-01-30 15:00:22 -0500

    Create day4RockPaper.py

---
+++ day4RockPaper.py
@@ -0,0 +1,19 @@
+import random
+
+print("Welcome to the Rock and scissor game")
+youChoice=input("Enter Rock, Peper, or Scissor: R,P,S: ")
+choice=str[R,P,S]
+
+computerChoice = random.randstr(choice)
+print(computerChoice)
+if youChoice==computerChoice:
+  print("You are tie")
+elif youChoice("R") and computerChoice ("S"):
+  print("You win")
+elif youChoice("P") and computerChoice ("R"):
+  print("You win")
+elif youChoice("S") and computerChoice ("P"):
+  print("You win")
+else:
+  print("You lose")
+  
