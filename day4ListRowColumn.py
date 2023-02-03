e31d269263ca129d1d6f1571bfe2422d80306f2e
Nan227 <you@example.com>
2023-02-03 18:22:04 -0500

    change the code on 3 files

--- day4ListRowColumn.py
+++ day4ListRowColumn.py
@@ -7,9 +7,11 @@ map = [row1, row2, row3]
 print(f"{row1} \n{row2} \n{row3}")
 position = input (" Where do you want to input 'x'? :")
 #write the code
-horizonal = int(position[0])
-vertical = int(position[1])
-serect_row= map[vertical-1]
-serect_row[horizonal-1] = "X"
-
+#horizonal = int(position[0])
+#vertical = int(position[1])
+#serect_row= map[vertical-1]
+#serect_row[horizonal-1] = "X"
+y= int(position[0])
+x= int(position[1])
+map[x-1][y-1]="o"
 print(f"{row1} \n{row2} \n{row3}")
--- day4RockPaper.py
+++ day4RockPaper.py
@@ -1,29 +1,69 @@
-print("\nWelcome to the Rock and scissor game")
-youChoice=str(input("Enter Rock, Peper, or Scissor: R,P,S: "))
-choice=["R","P","S"]
+import random
 
-computerChoice = random.randint(0,3)
-print(f"Computer choose {computerChoice} ")
-if computerChoice ==0:
-  computerChoice = "R"
-  
-elif computerChoice ==1:
-  computerChoice = "P"
-else:
-  computerChoice = "S"
+print("\nWelcome to the Rock and scissor game")
+youChoice=int(input("Enter '0' for Rock, '1' for Peper, or '2' for Scissor:  "))
+#rock
+rock ='''Rock
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+
+ '''
+#paper
+paper='''Paper
+    _______
+---'   ____)______
+      (____________)
+      (_______________)
+      (____________)
+---.__(________)
+
+ ''' 
+# Scissors
+scissor='''Scissor
+    _______
+---'   ____)________
+       _____________)
+       ________________)
+      (____)
+---.__(___)
+'''
+computerChoice = random.randint(0,2)
+
 # check the computer choice
-print(f"Computer chooses {computerChoice} and your chooses {youChoice} \nthe reasult is ")
+
+print(f"Computer chooses {computerChoice} ")
 # compair your choice and computure choice it who is win
+if computerChoice==0:
+  print(rock)
+elif computerChoice==1:
+  print(paper)
+else:
+  print(scissor) 
+print(f"and your chooses {youChoice}  ")
+# compair your choice and computure choice it who is win
+if youChoice==0:
+  print(rock)
+elif youChoice==1:
+  print(paper)
+else:
+  print(scissor)
+print(f"the reasult is ")
+#check 
 if youChoice==computerChoice:
   print("You are tie")
+
   #youChoice[R] and computerChoice[S]
-elif youChoice=="R" and computerChoice=="S":
+elif youChoice== 0 and computerChoice==2:
   print("You win")
   #youChoice[P] and computerChoice[R]
-elif youChoice=="P" and computerChoice=="R":
+elif youChoice==2 and computerChoice==1:
   print("You win")
   #youChoice[S] and computerChoice[P]
-elif youChoice=="S" and computerChoice=="P":
+elif youChoice==2 and computerChoice==1:
   print("You win")
 else:
   print("You lose")
