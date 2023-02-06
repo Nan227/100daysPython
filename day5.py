3051ac9643cc55e0586a095f3a71c3445eb3190d
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-02-06 13:37:13 -0500

    Added for loop on the name of classmade

--- day5.py
+++ day5.py
@@ -1,6 +1,9 @@
 import random
 
-name=input("Enter you classmades' name")
-numberName = name.split(" ")
-numberName = len(int(numberName))
-print(f" You have {numberName} classmades")
+name=input("Enter you classmades' names: ")
+nName = name.split(" ")
+print(f"Your classmades are: {nName}")
+for nameEach in nName:
+  print(nameEach + " is grumpy")
+numberName = int(len(nName))
+print(f"\nYou have {numberName} classmades")
