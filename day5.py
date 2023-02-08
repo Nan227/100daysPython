e3cfc2e4f8a8e02f52bc5135b4a454c9ce054486
Piyaporn Puangprasert (Nan) <dngooddeals@gmail.com>
2023-02-07 23:47:46 -0500

    update .append how the difference in to cases

--- day5.py
+++ day5.py
@@ -7,3 +7,19 @@ for nameEach in nName:
   print(nameEach + " is grumpy")
 numberName = int(len(nName))
 print(f"\nYou have {numberName} classmades")
+
+############################################
+
+newfriend = [ "Steve","Jooby","Casy"]
+addfriend1 = nName+newfriend
+print(addfriend1*3)
+print(addfrined1[0])
+print(addfrined1[0][1])
+####################################
+
+addfriened2 = nName.append(newfriend)
+print(addfriened2*3)
+print(addfrined2[0])
+print(addfrined2[0][1])
+###################################
+
