3e5fc034a66d7cf706b92b70a7bae861859f73f6
Piyaporn Puangprasert (Nan) <dngooddeals@gmail.com>
2023-02-09 22:45:55 -0500

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,16 @@
+import turtle
+
+tao=turtle
+tao.shape('turtle')
+tao.forward(100)
+tao.backward(180)
+tao.clear()
+
+for i in range(6):
+  tao.forward(50)
+  tao.left(45)
+  
+tao.clear()
+for i in range(8):
+  tao.forword(50)
+  tao.left(45)
