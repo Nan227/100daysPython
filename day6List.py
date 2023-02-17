56e919e3646f3db5b9d9ac279cfbb67d09da14a4
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-02-16 23:57:44 -0500

    convert string to int didnot work yet

--- day6List.py
+++ day6List.py
@@ -2,12 +2,13 @@ classmade = ["Nan", "Larry","Jamie","Bogda"]
 for i in classmade:
   print(i)
 print("-----------------------------------")
-student_heights = input("Input student hights then , :")
-for n in range(0, len(student_heights)):
-  highSlit = student_heights.split(",")
-  # student_heights[n]=int(nameSlit)
-print(highSlit)
-highSlitInt =int(highSlit)
-for i in range(0,len(highSlitInt)):
-  totalhigh = highSlitInt[i]+highSlitInt[i+1]
+student_heights = input("Input student hights: ").split()
+#for n in range(0, len(student_heights)):
+  #student_heights[n]=int(student_heights[n])
+print(student_heights)
+#for i in range(0,len(student_heights)):
+totalhigh = sum(student_heights)
+num_students=len(student_heights)
+averagehigh=totalhigh/num_students
 print(totalhigh)
+print(averagehigh)
