5f0433f313d2fafac2661b4e2a12ee0bcef30a75
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-02-19 22:18:50 -0500

    Add sum of even number code

--- day5higtMaxMinList.py
+++ day5higtMaxMinList.py
@@ -2,7 +2,7 @@ students_height=input("Enter students height 'cm': ").split()
 for n in range(0, len(students_height)):
   students_height[n] =int(students_height[n])
 print(students_height)
-
+print(type(students_height))
  #totalHeight =sum(student_height)
 totalHeight=0
 for height in students_height:
@@ -34,3 +34,9 @@ for height in students_height:
   if height < minHeight:
      minHeight=height
 print(f" The lowest of students height is {minHeight}")
+
+#sum 100 number only even number
+totalN = 0
+for n in range(0, 101, 2):
+  totalN += n
+print(f" Sum of 1-100 only even number is {totalN}" )
