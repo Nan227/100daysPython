db9d7281c1fb3873b940dd0ce26df48791d88cf0
Piyaporn Puangprasert (Nan) <dngooddeals@gmail.com>
2023-03-06 00:19:24 -0500

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,16 +1,22 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
-def  turn_left(): 
-  move()
-  move()
- 
-  
-def  jump():
-  move()
-  turn_left()
-  move()
-  turn_left()
-  move()
-  turn_left()
- 
-while not at_goal:
-  jump()
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+   
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
