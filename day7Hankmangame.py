b58b24799b32bc57f37214bdbc597ce05dda24f7
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-03-17 23:35:43 -0400

    check guess == answer now

--- day7Hankmangame.py
+++ day7Hankmangame.py
@@ -2,7 +2,22 @@ import random
 #step1 choose the random word
 list_word=["kanda","nareda","benjamin","anmieme","akane"]
 answer_word = random.choice(list_word)
-#print(answer_word)
+#print(answer_word
+
+number_line = len(answer_word)
+print(number_line)
+for i in range(0,number_line+1):
+  line ="_" *i
+print("The word is "+line)
+
 #step2 asking user
 guess=input("Guess letter: ").lower()
-print(guess)
+#print(guess)
+#while guess != answer_word:
+  #print(guess)
+for j in answer_word:
+  if guess == j:
+    result = True
+  else:
+    result = False
+print(result)
