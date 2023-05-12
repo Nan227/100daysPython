1453d9393ab282a7c3098fd244c350e9c55b5ff1
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-05-12 11:28:23 -0400

    7.2 code update

--- day7hanhman_v2.py
+++ day7hanhman_v2.py
@@ -2,13 +2,18 @@ import random
 # step 1  random word
 word_list = ["wordlist", "football", "watermelon", "smile","lucky"]
 chosen_word = random.choice(word_list)
-
+wordlength = range(len(chosen_word))
 #step2
 guess = input ("Guess a letter: ").lower()
-
+print(f' the solution word is {chosen_word}')
 #step3 check if the letter == chosen_word
-for letter in chosen_word:
+display = []
+for _ in wordlength:
+  display += "_"
+print(display)
+
+for position in wordlength:
+  letter =chosen_word[position]
   if letter == guess:
-    print ("Right")
-  else:
-    print("Wrong")
+    display[position] = letter
+ 
