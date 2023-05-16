b1e3dd66ac16e4e0fc85bee1f84997f01ce03fc9
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-05-15 23:48:58 -0400

    day7hangman version 2

--- day7hanhman_v2.py
+++ day7hanhman_v2.py
@@ -4,7 +4,7 @@ word_list = ["wordlist", "football", "watermelon", "smile","lucky"]
 chosen_word = random.choice(word_list)
 wordlength = range(len(chosen_word))
 #step2
-guess = input ("Guess a letter: ").lower()
+
 print(f' the solution word is {chosen_word}')
 #step3 check if the letter == chosen_word
 display = []
@@ -12,8 +12,9 @@ for _ in wordlength:
   display += "_"
 print(display)
 
+guess = input ("Guess a letter: ").lower()
 for position in wordlength:
   letter =chosen_word[position]
   if letter == guess:
     display[position] = letter
- 
+print(display)
